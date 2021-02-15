747cfb64b9774cccb42017a091e3610d8d5c2637
Staroscky <ederson.staroscky@gmail.com>
2021-02-15 00:03:10 -0300

    teste

--- database/index.js
+++ database/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 //Conexao com o mongodb Atlas que é o online
 //mongoose.connect('mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false', {
-mongoose.connect('mongodb+srv://SYSDBA:MASTER@cluster0.1jkkz.mongodb.net/gilsonQA', {
+mongoose.connect('mongodb+srv://SYSDBA:MASTER@cluster0.1jkkz.mongodb.net/gilson', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(()=>{
